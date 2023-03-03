c50c9abbce7905529f482de5d58eff610e24731e
bagusuntoro <untorobagus65@gmail.com>
2023-03-03 23:53:46 +0700

    add migrations and controller

---
+++ app/Models/Destination.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'name',
+        'address',
+        'address_url',
+        'description',
+        'area_id',
+        'user_id'
+    ];
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_03_01_164018_destination.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Destination extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destination', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('address_url');
+            $table->text('description');
+            $table->unsignedBigInteger('area_id');
+            // $table->foreign('area_id')->references('id')->on('area')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destination');
+    }
+}
