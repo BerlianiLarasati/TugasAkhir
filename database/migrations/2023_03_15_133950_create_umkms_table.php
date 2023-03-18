ab38de2b5f3e00a9b31439a639407cc4bc64d9ae
putualthea <altheaowl@gmail.com>
2023-03-18 23:12:00 +0700

    update

--- database/migrations/2023_03_15_133950_create_umkms_table.php
+++ database/migrations/2023_03_15_133950_create_umkms_table.php
@@ -16,6 +16,13 @@ class CreateUmkmsTable extends Migration
         Schema::create('umkms', function (Blueprint $table) {
             $table->id();
             $table->string("nama");
+            $table->string("address");
+            $table->string("shop_name");
+            $table->string("website");
+            $table->text("description");
+            $table->char("contact");
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('umkms_category')->onDelete('cascade');
             $table->timestamps();
         });
     }
--- resources/views/umkm.blade.php
+++ resources/views/umkm.blade.php
@@ -6,22 +6,30 @@
     <h1>PRODUK UMKM</h1>
 </div>
 
-<div class="grid">
-    <form action="{{route("umkm")}}" method="get">
-        <input type="text" name="search">
-        <button type="submit">search</button>
-    </form>
-    @foreach ($products as $product)
-    <div class="col">
-        <a href="#">
-            <img src="https://lh3.googleusercontent.com/p/AF1QipNA3g8VUZlf8uIr9ldG59Lp0Gu0hQh2RHwKuKqp=s1280-p-no-v1" alt="">
-            <div class="label">Food & Beverages</div>
-            <h3 style="margin: 10px 0px">{{$product->nama}}</h3>
-            <i class="fas fa-store"></i>
-            <span>Aqisa Rumah Rosella</span>
-        </a>
+<div class="container_umkm">
+
+    <div class="filter-keyword">
+        <form action="{{route("umkm")}}" method="get">
+            <input type="text" name="search">
+            <button type="submit">search</button>
+        </form>
     </div>
-    @endforeach
+
+    <div class="grid">
+        @foreach ($products as $product)
+        <div class="col">
+            <a href="{{route("umkm-detail")}}">
+                <div class="img-container">
+                    <img src="https://lh3.googleusercontent.com/p/AF1QipNA3g8VUZlf8uIr9ldG59Lp0Gu0hQh2RHwKuKqp=s1280-p-no-v1" alt="">
+                </div>
+                <div class="label">Food & Beverages</div>
+                <h3 style="margin: 10px 0px">{{$product->nama}}</h3>
+                <i class="fas fa-store"></i>
+                <span>Aqisa Rumah Rosella</span>
+            </a>
+        </div>
+        @endforeach
+    </div>
 </div>
 
 @endsection
