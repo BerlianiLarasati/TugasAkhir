c3ff15db0387f795c685bf42160b5c95aeaca957
putualthea <altheaowl@gmail.com>
2023-03-20 12:42:06 +0700

    update

---
+++ resources/views/destinasi-detail.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.navfoot')
+@section('section')
+
+   <div class="desc">
+        <div class="namepage">
+            <a href="">Beranda > Destinasi Wisata</a>
+            <h1>MUSEUM PENDIDIKAN SURABAYA</h1>
+        </div>
+        <img src="https://tourism.surabaya.go.id/storage/tour/1648709900_2.jpg" alt="">
+        <div class="desc">
+            <h3>Museum Pendidikan Surabaya</h3>
+            <div class="kategori">
+                <div class="kat">
+                    <span class="material-symbols-outlined">
+                        location_on
+                        </span>
+                    <h6>Jl. Genteng Kali No.10</h6>
+                </div>
+                <div class="kat">
+                    <span class="material-symbols-outlined">
+                        category
+                        </span>
+                    <h6>Museum</h6>
+                </div>
+            </div>
+            <p>Museum Pendidikan Surabaya merupakan museum tematik yang didirikan sebagai langkah pelestarian sejarah dan budaya bangsa dengan tujuan untuk mendukung kegiatan edukasi, riset dan rekreasi di Kota Surabaya. Diresmikan oleh Walikota Surabaya Tri Rismaharini pada 25 November 2019. yang menyimpan bukti materiil Pendidikan pada masa Pra-Aksara, Masa Klasik, Masa Kolonial dan Masa Kemerdekaan. Berlokasi di Jalan Genteng Kali No 10 Surabaya yang merupakan EX Sekolah Taman siswa.</p>
+        <p>ZONA MASA PRA-AKSARA Zona Pendidikan di era ini mungkin terlihat sangat sederhana dan mendasar, tapi juga mengajarkan salah satu esensi paling utama pengetahuan yang tetap relevan hingga era kini yaitu bertahan hidup. Didalam zona ini terdapat koleksi Manuskrip Kuno Era1800-an , Manuskrip berbahan dasar Lontar dan Diorama Pra Aksara (Manusia Purba)</p>
+        <p>ZONA KERAJAAN Pada zona ini Pengaruh dan corak dari luar nusantara yang berasimilasi dengan elemen lokal menjadikan pendidikan di era ini menjadi sangat dinamis dan menarik dibanding sebelumnya. Berbagai ajaran agama dan pengetahuan yang disebarkan inipun memiliki jenis aksara yang masing-masing.</p>
+        <p>ZONA KOLONIAL Pada Pendidikan masa ini menceritakan tentang Pendidikan era Kolonial Belanda dan Jepang. Koleksi terkait zona Kolonial ini meliputi Sepeda Guru. Sepeda Zundapp untuk pendampingan Orang tua mengantar siswa berangkat Sekolah dsb.</p>
+        <p>ZONA KEMERDEKAAN. Pada Zona Kemerdekaan ini menampilkan Koleksi –koleksi terkait benda bukti materiil peradaban periodisasi tahun 1945 – 1990an.</p>
+    </div>
+
+ @endsection
